0f61c8dcfe7739bf5441b2cba6e14144b4d90cd3
Balázs Kiss <dev@balazskiss.name>
2019-12-11 00:57:45 +0100

    lowercase

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ let package = Package(
     ],
     dependencies: [
         // Module map for `libmysql`
-        .Package(url: "https://github.com/vapor/cmysql.git", from: "2.0.2"),
+        .package(url: "https://github.com/vapor/cmysql.git", from: "2.0.2"),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", from: "2.1.5"),
+        .package(url: "https://github.com/vapor/node.git", from: "2.1.5"),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks
-        .Package(url: "https://github.com/vapor/core.git", from: "2.2.1"),
+        .package(url: "https://github.com/vapor/core.git", from: "2.2.1"),
 
         // JSON parsing and serialization for storing arrays and objects in MySQL
-        .Package(url: "https://github.com/vapor/json.git", from: "2.2.2"),
+        .package(url: "https://github.com/vapor/json.git", from: "2.2.2"),
     ],
     targets: [
         .target(name: "MySQL", dependencies: ["CMySQL", "Node", "Core", "JSON"])
